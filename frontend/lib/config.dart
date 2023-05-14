eee1f4e6ebe70f4712c0608349d42301b776bc31
AashrayPrajapati <aashray333@gmail.com>
2023-05-14 15:19:28 +0545

    made app dynamic except image

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -1,3 +1,6 @@
 // var apiUrl = 'http://10.0.2.2';
 
-// var apiUrl = 'http://100.22.1.130';
+// var apiUrl = 'http://100.22.1.130'; //
+
+// var apiUrl = 'http://100.22.1.130'; // college
+// var apiUrl = 'http://192.168.31.116'; // another one
